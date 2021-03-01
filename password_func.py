d2d389321cf4e1e342fb19431f23f5970fce808e
kayn0111034 <0111034@student.isf.edu.hk>
2021-03-02 02:18:58 +0800

    Added new password generator (doesn't work)
    password_func: coded function that returns TypeError
    password_generator: created input paths

--- password_func.py
+++ password_func.py
@@ -2,7 +2,7 @@ import collections
 import string
 import random
 
-def password_gen(letter_amount):
+def password_gen(char_amount):
     letters=collections.deque(string.ascii_letters)
     numbers=collections.deque(string.digits)
     characters=collections.deque(letters+numbers)
@@ -12,6 +12,31 @@ def password_gen(letter_amount):
     for i in range(letter_amount):
         p=random.choice(characters)
         password.append(p)
+
+    password=''.join(password)
+    print(password)
+
+def password_with_num_gen(char_amount, number_amount):
+    letters=collections.deque(string.ascii_letters)
+    numbers=collections.deque(string.digits)
+
+    password=[]
+    pass_num=[]
+    insert_ord=0
+
+    for i in range(char_amount-number_amount):
+
+        password_letters=random.choice(letters)
+        password.append(password_letters)
+
+    for i in range(number_amount):
+        password_numbers=random.choice(numbers)
+        pass_num.append(password_numbers)
+
+    for i in range(number_amount):
+        password.insert(pass_num[insert_ord],random.randint(0,char_amount))
+        #returns TypeError: 'str' object cannot be interpreted as an integer
+        insert_ord+=1
 
     password=''.join(password)
     print(password)
--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,19 @@
-from password import password_gen
+from password_func import *
 
+print("(0) Random Password Generator")
+print("(1) Random Password Generator but You Can Choose the Amount of Numbers In It")
 
-print("Create a randomly generated password!")
-letter_amount=int(input("Input how long you would like your password to be:"))
+choose=int(input("Which password generator do you want to use?"))
 
-password_gen(letter_amount)
+if choose==0:
+    print("Create a randomly generated password!")
+    char_amount=int(input("Input how long you would like your password to be:"))
+
+    password_gen(char_amount)
+
+if choose==1:
+    print("Create a randomly generated password with amount of numbers")
+    char_amount=int(input("Input how long you would like your password to be:"))
+    number_amount=int(input("Input how many numbers you would like in your password:"))
+
+    password_with_num_gen(char_amount,number_amount)
